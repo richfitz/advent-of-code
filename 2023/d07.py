013631da00fe7d02c2ce9c124cf7e1a33425b744
Rich FitzJohn <r.fitzjohn@imperial.ac.uk>
2023-12-07 18:03:13 +0000

    Simplify solution

--- 2023/d07.py
+++ 2023/d07.py
@@ -1,41 +1,25 @@
-from collections import defaultdict
-
-
 def parse_input(path):
-    ret = []
     with open(path) as f:
-        for x in f.readlines():
-            ret.append(x.strip().split())
-    return ret
+        return [x.strip().split() for x in f.readlines()]
+
+
+def index_of_max(x):
+    return x.index(max(x))
 
 
 def parse_hand(hand, order):
-    values = [order.index(card) for card in hand]
-    table = defaultdict(lambda : 0)
+    values = [12 - order.index(card) for card in hand]
+    counts = [0] * len(order)
     for i in values:
-        table[i] += 1
-    res = sorted(table.items(), key=lambda x: (-x[1], x[0]))
-    if order[12] != "J" or table[12] == 0 or table[12] == 5:
-        best = res[0]
-    else:
-        best = res[0] if len(res) == 1 or res[0][0] != 12 else res[1]
-        best = best[0], best[1] + table[12]
-        table[12] = 0
-    n = len([x for x in table.values() if x > 0])
-    if n == 1:
-        score = 0
-    elif n == 2: # full house or four of a kind
-        score = 1 + int(best[1] != 4)
-    elif n == 3: # three of a kind or two pair
-        score = 3 + int(best[1] != 3)
-    else:        # two of a kind or a big pile of nothing
-        score = 5 + int(best[1] != 2)
-    return [score, *values, hand]
+        counts[i] += 1
+    if order[12] == "J" and counts[0] > 0 and counts[1] < 5:
+        counts[index_of_max(counts[1:]) + 1] += counts[0]
+        counts[0] = 0
+    return sorted(counts, reverse=True) + values
 
 
 def score(d, order):
-    res = [(parse_hand(hand, order), bid) for hand, bid in d]
-    res.sort(key=lambda x: x[0], reverse=True)
+    res = sorted([(parse_hand(hand, order), bid) for hand, bid in d])
     return sum((i + 1) * int(x[1]) for i, x in enumerate(res))
 
 
