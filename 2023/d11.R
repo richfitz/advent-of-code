66f02d9e42afa773de6d587a8d996e1b68b8ef68
Rich FitzJohn <r.fitzjohn@imperial.ac.uk>
2023-12-11 09:39:50 +0000

    Import r solution

---
+++ 2023/d11.R
@@ -0,0 +1,32 @@
+parse_input <- function(path) {
+  m <- strsplit(readLines(path), NULL)
+  pairs <- which(matrix(unlist(m) == "#", length(m), byrow = TRUE), TRUE)
+  offset <- apply(pairs, 2, function(x) {
+    cumsum(c(FALSE, diff(findInterval(seq_len(max(x)), sort(unique(x)))) == 0))
+  })
+  list(pairs = pairs,
+       offset = cbind(offset[pairs[, 1], 1], offset[pairs[, 2], 2]))
+}
+
+distances <- function(d, n) {
+  sum(dist(d$pairs + d$offset * (n - 1), "manhattan"))
+}
+
+part1 <- function(d) {
+  distances(d, 2)
+}
+
+part2 <- function(d) {
+  distances(d, 1000000)
+}
+
+test <- parse_input("test-input-d11.txt")
+stopifnot(part1(test) == 374)
+
+stopifnot(distances(test, 2) == 374)
+stopifnot(distances(test, 10) == 1030)
+stopifnot(distances(test, 100) == 8410)
+
+d <- parse_input("input-d11.txt")
+print(part1(d))
+print(part2(d))
